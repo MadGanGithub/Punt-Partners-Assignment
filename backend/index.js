4c25585c6d784c40d3fb601b95c6b324204bec09
MadGanGithub <madhavganesan95@gmail.com>
2024-07-22 12:22:20 +0530

    backend settings

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
-import app from "./app.js"; 
- 
-app.listen(4000, ()=>{ 
-    console.log("Started at port: 4000"); 
-}); 
+import dotenv from "dotenv";
+import app from "./app.js";
+
+dotenv.config({ path: "./config/config.env" });
+
+app.listen(process.env.PORT, ()=>{ 
+    console.log(`Started at port: ${process.env.PORT}`);
+});
