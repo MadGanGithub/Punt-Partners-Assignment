26320c03aa65fc6df327a8e9ef823cd8876356fb
MadGanGithub <madhavganesan95@gmail.com>
2024-07-22 12:26:53 +0530

    trailing slash

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ import userRoutes from "./routes/routes.js";
 const app=express();
 
 app.use(cors({
-   origin: 'https://punt-frontend-puce.vercel.app/', 
+   origin: 'https://punt-frontend-puce.vercel.app', 
    credentials: true,
    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
    allowedHeaders: ['Content-Type', 'Authorization'], 
