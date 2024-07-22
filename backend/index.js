b8aee011b7f8dd6a99672b57a1bda0fc547396d7
MadGanGithub <madhavganesan95@gmail.com>
2024-07-22 18:13:38 +0530

    removed mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
 import app from "./app.js";
-import connectionDB from "./config/database.js";
+// import connectionDB from "./config/database.js";
 
 dotenv.config({ path: "./config/config.env" });
 
-connectionDB();
+// connectionDB();
 
 app.listen(process.env.PORT, ()=>{ 
     console.log(`Started at port: ${process.env.PORT}`);
